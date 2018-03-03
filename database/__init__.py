61d967ff6b88a2c71b3a787b9255e1d51965d8c8
Elijah Bendinsky <syther618@gmail.com>
2018-03-03 18:09:47 -0500

    Database init handling

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 import config
 
-db = SQLAlchemy()  # <-- Create SQL database
 engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
-
 conn = engine.connect()
--- database/database.py
+++ database/database.py
@@ -1,5 +1,24 @@
-from database import db, engine
+from database import engine, conn
+import logging
 
 
-class Tournament(db.Model):
-    __tablename__ = 'tournament'
+def init_database(script):
+    if engine.has_table("tournament") and engine.has_table("rules") and engine.has_table("match") and engine.has_table("team"):
+        logging.info("Database exists, and is proper")
+        return True
+    transaction = conn.begin()
+    try:
+        init_script = open(script, 'r')
+        for line in init_script.read():
+            transaction.execute(line)
+        init_script.close()
+        transaction.commit()
+        logging.info("Database has been setup")
+    except IOError:
+        logging.error("Creation script not found")
+        raise
+    except:
+        transaction.rollback()
+        logging.error("Generic database error")
+        raise
+    return True
