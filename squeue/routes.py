ba069700f0e2881c7010f081e00b0850f6830f27
Elijah Bendinsky <syther618@gmail.com>
2018-04-17 15:51:40 -0400

    Disable caching

--- squeue/routes.py
+++ squeue/routes.py
@@ -1,9 +1,11 @@
 from squeue import squeue_blueprint
 from flask import render_template, session
 from squeue.queue import SQueue
+from squeue.nocache import nocache
 
 
 @squeue_blueprint.route('/', methods=['GET'])
+@nocache
 def get_squeue():
     if 'players' not in session.keys():
         session['players'] = SQueue().players
